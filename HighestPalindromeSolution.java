0e8325cb61c04c24fdaf19d7923f156e45f2fda6
Irfan Adi <irfan.adi1209@gmail.com>
2023-08-14 20:59:59 +0700

    solving HighestPalindromeSolution

--- HighestPalindromeSolution.java
+++ HighestPalindromeSolution.java
@@ -1,10 +1,27 @@
 public class HighestPalindromeSolution {
     public static void main(String[] args) {
-        String test = "3943";
-        String result = findPalindrome(test, 1);
+        String test = "3994929345214993";
+        String result = findPalindrome(test, 2);
         System.out.println(result);
     }
 
     private static String findPalindrome(String test, int replacement) {
+        if (test.length() == 0 || test.length() == 1) return test; // handle empty or 1 length string.
+        int stringLength = test.length();
+        char first = test.charAt(0), last = test.charAt(stringLength - 1); // get first and last char
+        if (replacement > 0 && first != last) { // before continue, check if first and last is not same and check replacement count
+            replacement--;
+            int firstInt = Character.getNumericValue(first);
+            int lastInt = Character.getNumericValue(last);
+            char max = (char) (Math.max(firstInt, lastInt) + '0'); // check max of first or last
+            // set max value between first and last
+            first = max;
+            last = max;
+            // after this, first and last is same so can check palindrome again;
+        }
+        String palindromeResult = findPalindrome(test.substring(1, stringLength - 1), replacement);
+        // if result found "-1", return "-1" for all recursion, else return palindrome value
+        if (first == last && !palindromeResult.equalsIgnoreCase("-1")) return first + palindromeResult + last;
+        return "-1";
     }
 }
--- WeightedStringsSolution.java
+++ WeightedStringsSolution.java
@@ -13,21 +13,22 @@ public class WeightedStringsSolution {
     }
 
     private static String[] weightedStrings(String test, int[] queries) {
-        String testLowerCase = test.toLowerCase();
+        String testLowerCase = test.toLowerCase(); // lower case so A-Z can be checked too
         Map<String, Integer> map = new HashMap<>();
-        String currentChar = "";
-        for (int i = 0; i < testLowerCase.length(); i++) {
-            if (currentChar.contains(String.valueOf(testLowerCase.charAt(i)))) {
-                String oldChar = currentChar;
-                currentChar += String.valueOf(testLowerCase.charAt(i));
-                map.put(currentChar, map.get(oldChar) + (testLowerCase.charAt(i) - 96));
+        String lastChar = ""; // string lastChar initialization
+        for (int i = 0; i < testLowerCase.length(); i++) { // checking letter one by one
+            char currentChar = testLowerCase.charAt(i);
+            if (lastChar.contains(String.valueOf(currentChar))) { // if lastChar contains currentChar
+                String prevChar = lastChar; // set prevChar so the map can get value of prevChar
+                lastChar += String.valueOf(currentChar); // add character to string
+                map.put(lastChar, map.get(prevChar) + (currentChar - 96)); // insert with previous value
             } else {
-                currentChar = String.valueOf(testLowerCase.charAt(i));
-                map.put(currentChar, testLowerCase.charAt(i) - 96);
+                lastChar = String.valueOf(currentChar); // currentChar is different from lastChar, set lastChar to currentChar
+                map.put(lastChar, currentChar - 96); // insert new value
             }
         }
         String[] result = new String[queries.length];
-        for (int i = 0; i < queries.length; i++) {
+        for (int i = 0; i < queries.length; i++) { // display result yes or no
             result[i] = map.containsValue(queries[i]) ? "Yes" : "No";
         }
         return result;
