0ecc280fa1f60a6d429168ce01552ebb9a2eee34
Irfan Adi <irfan.adi1209@gmail.com>
2023-08-15 09:59:53 +0700

    Solving BalancedBracketSolution and add README.md

--- BalancedBracketSolution.java
+++ BalancedBracketSolution.java
@@ -1,3 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class BalancedBracketSolution {
-    
+    public static void main(String[] args) {
+        String test = "{ ( ( [ ] ) [ ] ) [ ] }";
+        String result = isBalancedBracket(test);
+        System.out.println(result);
+    }
+
+    private static String isBalancedBracket(String test) {
+        Deque<Character> deque = new ArrayDeque<>(); // LIFO (Last In First Out)
+
+        char[] testCharArr = test.toCharArray();
+
+        for (int i = 0; i < testCharArr.length; i++) {
+            if (
+                testCharArr[i] == '{' ||
+                testCharArr[i] == '[' ||
+                testCharArr[i] == '('
+            ) { // check if char at i is open bracket or not
+                deque.push(testCharArr[i]); // push to deque
+            } else if (
+                !deque.isEmpty() && ( // closed without open bracket will not be checked
+                    // pairing closed bracket with open bracket
+                    (testCharArr[i] == '}' && deque.peekFirst() == '{') ||
+                    (testCharArr[i] == ']' && deque.peekFirst() == '[') ||
+                    (testCharArr[i] == ')' && deque.peekFirst() == '(')
+                )
+            ) {
+                deque.pop(); // delete first deque
+            }
+        }
+        
+        return deque.isEmpty() ? "YES" : "NO"; // check if deque is not empty then bracket is not balanced, return NO. Otherwise return YES.
+    }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# GITS-HITOPIA Problem Solving Test
+
+## BalancedBracketSolution.java
+
+Ukuran Kompleksitas Kode:
+
+-   Dalam kode tersebut hanya menggunakan 1 perulangan berdasarkan panjang dari string yang akan di-test (n). Time Complexity dari kode tersebut adalah O(n).
+-   Dalam kode tersebut melakukan cek pada string tersebut. Worst-case dalam kode tersebut adalah jika semua karakter di string tersebut hanya berisikan open-bracket `(contoh: '{[([({')`. Space Complexity dari kode tersebut adalah O(n).
